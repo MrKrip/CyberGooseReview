64527da0e0bcafa9d6818682a83161582d271d05
Павел Велетнюк <mrkripster4@gmail.com>
2022-10-12 11:53:19 +0300

    Add 2 layers

---
+++ DAL/Context/DefaultContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAL.Context
+{
+    public class DefaultContext : IdentityDbContext
+    {
+        public DefaultContext(DbContextOptions<DefaultContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
